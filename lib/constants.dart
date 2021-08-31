c5d3b4ccd993548d29fb5d7abb9aa478a609833a
AxelBlaz3 <karthikgaddam4@gmail.com>
2021-08-31 17:06:27 +0530

    Add constants for API and navigation
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+// API
+const String kBaseUrl = '';
+
+// Route names
+const String kLoginRoute = '/login';
+const String kSignupRoute = '/signup';
+const String kHomeRoute = '/home';
+const String kRecipeRoute = '/recipe';
+const String kProfileRoute = '/profile';
+const String kUploadRecipe = '/upload';
+const String kEditProfileRoute = '/editProfile';
+const String kResetPasswordRoute = '/resetpassowrd';
+const String kOnboardingRoute = '/onboarding';
